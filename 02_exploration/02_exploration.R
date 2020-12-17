b7a3a921ca28c7c95f5c318cec1f3c7b70e88949
Florian Stracke <flostracke@gmail.com>
2020-12-17 17:00:11 +0100

    progress

--- 02_exploration/02_exploration.R
+++ 02_exploration/02_exploration.R
@@ -27,6 +27,26 @@ mailchimp_users_tbl
 
 # DATA PREPARATION ---- 
 
+google_analytics_long_hour_tbl <- google_analytics_tbl %>% 
+  mutate(date = ymd_h(dateHour)) %>% 
+  select(-dateHour) %>% 
+  pivot_longer(cols = pageViews:sessions)
+
+google_analytics_long_hour_tbl
+
+subscribers_day_tbl <- mailchimp_users_tbl %>% 
+  summarise_by_time(
+    .date_var = optin_time, 
+    .by       = "day", 
+    optins    = n()
+  ) %>% 
+  pad_by_time(
+    .date_var  = optin_time,
+    .by        = "day", 
+    .pad_value = 0
+  )
+
+subscribers_day_tbl
 
 # 1.0 TIME SERIES PLOT ----
 # - Primary Visualization Tool
@@ -36,12 +56,27 @@ mailchimp_users_tbl
 
 # * Basics ----
 
+subscribers_day_tbl %>% 
+  plot_time_series(.date_var = optin_time, .value = optins)
+
+google_analytics_long_hour_tbl %>% 
+  plot_time_series(date, value)
+
 
 # * Facets/Groups ----
-
+google_analytics_long_hour_tbl %>% 
+  group_by(name) %>% 
+  plot_time_series(date, value)
 
 # * Mutations/Transformations ----
 
+
+subscribers_day_tbl %>% 
+  plot_time_series(.date_var = optin_time, .value = log(optins + 1))
+
+google_analytics_long_hour_tbl %>% 
+  group_by(name) %>% 
+  plot_time_series(date, log(value + 1))
 
 # * Smoother Adjustment
 
